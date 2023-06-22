1db7d1ebaa76601c218713806c4c27d908f2ae7d
gpalmarante <gpalmarante@gmail.com>
2023-06-21 21:54:35 -0300

    comecando navegação

--- RPGDev/Controler.cs
+++ RPGDev/Controler.cs
@@ -48,6 +48,30 @@ namespace RPGDev
 
             Console.ReadKey();
         }
+        public void OpcoesMap()
+        {
+            Console.WriteLine("Voce entrou no labirinto");
+            Console.WriteLine("Voce pode andar em 4 direcoes");
+            Console.WriteLine("digite 1 para ir para o Norte");
+            Console.WriteLine("digite 2 para ir para o SUL");
+            Console.WriteLine("digite 3 para ir para o Leste");
+            Console.WriteLine("digite 4 para ir para o Oest");
+            int opcao = int.Parse(Console.ReadLine());
+            if (ChecarCaminho(opcao))
+            {
+                Console.WriteLine(" eta nois");
+            }
+
+
+        }
+        public bool ChecarCaminho(int opcao)
+        {
+            if(opcao == 4 && P1.Localização[0] <= 0) { return false; }
+            if (opcao == 1 && P1.Localização[1] <= 0) { return false; }
+            if (opcao == 3 && P1.Localização[0] >= 11) { return false; }
+            if (opcao == 2 && P1.Localização[1] >= 11) { return false; }
+            return true;
+        }
 
     }
 }
