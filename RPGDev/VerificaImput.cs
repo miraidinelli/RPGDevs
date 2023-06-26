cd3d00fced1640dfdbc6349feffc7632480c2a3a
gpal <gpalmarante@gmail.com>
2023-06-26 15:05:09 -0300

    atualiza

---
+++ RPGDev/VerificaImput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGDev
+{
+    internal class VerificaImput
+    {
+
+        public int GetInput(int numeros)
+        {
+            int opcao = 0;  
+
+            return opcao;
+        }
+    }
+}
